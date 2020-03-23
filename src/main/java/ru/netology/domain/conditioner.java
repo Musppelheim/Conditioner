bc5fa1e6df9fb48f8ed1d08cb3d5c49907fa0ecf
Musppelheim <neloliver@hotmail.com>
2020-03-24 01:22:14 +0700

    conditioner added

--- src/main/java/ru/netology/domain/conditioner.java
+++ src/main/java/ru/netology/domain/conditioner.java
@@ -1,4 +1,26 @@
 package ru.netology.domain;
 
 public class conditioner {
+    private String name;
+    private int maxTemperature = 35;
+    private int minTemperature = 17;
+    private int currentTemperature = 20;
+    boolean on;
+
+    public void increaseCurrentTemperature() {
+            currentTemperature += 1;
+    }
+
+    public void decreaseCurrentTemperature() {
+            currentTemperature -= 1;
+    }
+
+    public int getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public void setCurrentTemperature(int currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+
 }
---
+++ src/test/java/ru/netology/domain/conditionerTest.java
@@ -0,0 +1,30 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class conditionerTest {
+    @Test
+    public void shouldCreate() {
+        conditioner conditioner = new conditioner();
+    }
+    @Test
+    public void shouldIncreaseCurrentTemperature() {
+        conditioner conditioner = new conditioner();
+        conditioner.increaseCurrentTemperature();
+        assertEquals(21, conditioner.getCurrentTemperature());
+    }
+    @Test
+    public void shouldDecreaseCurrentTemperature() {
+        conditioner conditioner = new conditioner();
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(19, conditioner.getCurrentTemperature());
+    }
+    @Test
+    public void shouldSetterLogic() {
+        conditioner conditioner = new conditioner();
+        conditioner.setCurrentTemperature(20);
+        assertEquals(20, conditioner.getCurrentTemperature());
+    }
+}
